e8efdaf105f50cbf3299a7fdacdd2c5104887a02
Audrey Ndum <audreyndum@gmail.com>
2021-12-30 21:32:01 +0100

    setup redux persist configurations

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,7 +4,7 @@ import './cart-item.styles.scss'
 const CartItem = ({item: { imageUrl, price, quantity }}) => {
     return (
         <div className="cart-item">
-            <img src={imageUrl} alt="Cart Item Image"></img>
+            <img src={imageUrl} alt="item"></img>
             <div className="item-details">
                 <span className="name"></span>
                 <span>{ quantity } x { price } CFA</span>
--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
-    </React.StrictMode>
-    </Provider>,
-  document.getElementById('root')
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
