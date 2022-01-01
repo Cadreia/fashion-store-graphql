40d9956eb2839360b6e0ebad44565035a7d02650
Audrey Ndum <audreyndum@gmail.com>
2022-01-01 10:52:51 +0100

    normalize shop collection data

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selector";
+import './collection.styles.scss'
 
 const CollectionPage = () => {
   const { collectionId } = useParams();
   const collection = useSelector(selectCollection(collectionId));
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h3>CollectionPage Page! for {collection.title}</h3>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
