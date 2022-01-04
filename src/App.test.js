5878864514fb156c8e2c59c15958a2d02538d746
Audrey <40563356+Cadreia@users.noreply.github.com>
2022-01-04 22:29:57 +0100

    Implement CI/CD and setup hosting configurations (#47)
    * create ci/cd configurations and resolve test error
    
    * fix node-sass package error
    
    * use older version that supports SFTP
    
    * setup alternate SFTP config
    
    * make project accessible from subdomain
    
    * switch prod branch from ci-cd to main

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { store, persistor } from "./redux/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders sneakers text", () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+  const linkElement = screen.getByText(/sneakers/i);
   expect(linkElement).toBeInTheDocument();
 });
